87d458e3d86155c67c76ef7d38f937fe3c411a01
Fallest <56403805+Fallest@users.noreply.github.com>
2022-02-01 00:01:42 +0100

    First commit
    D

---
+++ Proyecto6/src/Controller/OrderManager.java
@@ -0,0 +1,158 @@
+package Controller;
+
+import Model.Order;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class OrderManager {
+
+    /**
+     * Clase para realizar consultas sobre la tabla "orders" de la BD.
+     */
+    private static ResultSet orders;
+
+    public static void start(String where) {
+        // Inicializa el ResultSet de pedidos, para recorrerlo desde el navegador
+        // de pedidos.
+        try {
+            Connection con = DBConnection.getConnection();
+            Statement stmt = con.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+            orders = stmt.executeQuery("select * from orders " + where);
+        } catch (SQLException ex) {
+            System.out.println("ERROR: An exception ocurred at OrderManager.start().");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Funciones para recorrer el ResultSet">
+    public static Order first() {
+        try {
+            if (orders.first()) {
+                return new Order(
+                        orders.getInt(1),
+                        orders.getInt(2),
+                        orders.getFloat(3)
+                );
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERROR: An exception ocurred at OrderManager.first().");
+        }
+        return null;
+    }
+
+    public static Order last() {
+        try {
+            if (orders.last()) {
+                return new Order(
+                        orders.getInt(1),
+                        orders.getInt(2),
+                        orders.getFloat(3)
+                );
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERROR: An exception ocurred at OrderManager.last+().");
+        }
+        return null;
+
+    }
+
+    public static Order next() {
+        try {
+            if (orders.next()) {
+                return new Order(
+                        orders.getInt(1),
+                        orders.getInt(2),
+                        orders.getFloat(3)
+                );
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERROR: An exception ocurred at OrderManager.next().");
+        }
+        return null;
+    }
+
+    public static Order prev() {
+        try {
+            if (orders.previous()) {
+                return new Order(
+                        orders.getInt(1),
+                        orders.getInt(2),
+                        orders.getFloat(3)
+                );
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERROR: An exception ocurred at OrderManager.prev().");
+        }
+        return null;
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Funciones Select, Update, Delete e Insert">
+    public static ArrayList<Order> select(String where) {
+        ArrayList<Order> res = new ArrayList<>();
+
+        try {
+            Connection con = DBConnection.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rset = stmt.executeQuery("select * from orders " + where);
+
+            while (rset.next()) {
+                res.add(new Order(
+                        rset.getInt(1),
+                        rset.getInt(2),
+                        rset.getFloat(3)
+                ));
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("ERROR: An exception ocurred at OrderManager.select().");
+        }
+
+        return res;
+    }
+
+    public static void update(String what, String where) {
+        String query = "udpate orders set = " + what + " " + where;
+
+        try {
+            Connection con = DBConnection.getConnection();
+            Statement stmt = con.createStatement();
+            int rowsAffected = stmt.executeUpdate(query);
+
+            System.out.println(rowsAffected + " rows affected in the update.");
+        } catch (SQLException ex) {
+            System.out.println("ERROR: An exception ocurred at OrderManager.update()");
+        }
+    }
+
+    public static void delete(String where) {
+        String query = "delete orders " + where;
+
+        try {
+            Connection con = DBConnection.getConnection();
+            Statement stmt = con.createStatement();
+            int rowsAffected = stmt.executeUpdate(query);
+
+            System.out.println(rowsAffected + " rows affected in the delete.");
+        } catch (SQLException ex) {
+            System.out.println("ERROR: An exception ocurred at OrderManager.delete()");
+        }
+    }
+
+    public static void insert(String values) {
+        String query = "insert into orders values (" + values + ")";
+
+        try {
+            Connection con = DBConnection.getConnection();
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(query);
+        } catch (SQLException ex) {
+            System.out.println("ERROR: An exception ocurred at OrderManager.insert()");
+        }
+    }
+    // </editor-fold>
+}
